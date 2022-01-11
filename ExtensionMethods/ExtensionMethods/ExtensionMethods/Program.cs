78733f1cb34d0c06db95dc35e384cd52318f1d89
henrychiang <henrychiang@boyu66.net>
2022-01-11 10:26:39 +0800

    feature: extension methods

---
+++ ExtensionMethods/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "123456789";
+            Console.WriteLine(StringExtensionOne.Reverse(s));
+
+            Console.WriteLine(s.ReverseExtension());
+
+            int[] ints = { 10, 45, 15, 39, 21, 26 };
+            var result = ints.OrderBy(g => g);
+            foreach (var i in result)
+            {
+                System.Console.Write(i + " ");
+            }
+
+            string s2 = "Hello Extension Methods";
+            int i2 = s2.WordCount();
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/ExtensionMethods/StringExtensionOne.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    // static class is required
+    public static class StringExtensionOne
+    {
+        // 字串反轉
+        // using static method
+        public static string Reverse(string s)
+        {
+            if (String.IsNullOrEmpty(s))
+                return "";
+            char[] charArray = new char[s.Length];
+            int len = s.Length - 1;
+            for (int i = 0; i <= len; i++)
+            {
+                charArray[i] = s[len - i];
+            }
+            return new string(charArray);
+        }
+
+        // 字串反轉
+        // using static method
+        public static string ReverseExtension(this string s)
+        {
+            if (String.IsNullOrEmpty(s))
+                return "";
+            char[] charArray = new char[s.Length];
+            int len = s.Length - 1;
+            for (int i = 0; i <= len; i++)
+            {
+                charArray[i] = s[len - i];
+            }
+            return new string(charArray);
+        }
+    }
+
+    public static class MSDNMyExtensions
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' },
+                             StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
