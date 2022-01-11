559e085ab4d4e60203526f418e7138e012d5a1fe
henrychiang <henrychiang@boyu66.net>
2022-01-11 10:24:45 +0800

    feature: enum practice

---
+++ EnumExample/EnumExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EnumExample
+{
+    enum Flowers
+    {
+        None,
+        Daisy = 1,
+        Lili = 2,
+        Rose = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flowers Flower;
+
+            if (Enum.TryParse("3", out Flower))
+            {
+                Console.WriteLine(Flower);
+            }
+        }
+    }
+}
