66adf048ee40f41235e3412756c11877810910c7
henrychiang <grtert@gmail.com>
2022-02-28 00:30:22 +0800

    feature: EventPractice

---
+++ EventPractice/EventPractice1/EventPractice1/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+MySubscriber subscriber = new MySubscriber(); // <1>
+subscriber.SetText(); // <3>
+
+
+
+public delegate void TextChangedEventHandler(string oldValue, string newValue);
+public class MyPublisher
+{
+    private string _text;
+
+    public TextChangedEventHandler TextChanged;
+
+    public string Text
+    {
+        get { return _text; }
+        set { 
+            if(_text != value)
+            {
+                string old = _text;
+                _text = value;
+                TextChanged(old, _text);
+            }
+        }
+    }
+}
+
+public class MySubscriber
+{
+    MyPublisher publisher;
+
+    public MySubscriber()
+    {
+        this.publisher = new MyPublisher();
+        publisher.TextChanged = new TextChangedEventHandler(TextChangedHandler);
+    }
+
+    public void TextChangedHandler(string oldValue, string newValue)
+    {
+        Console.WriteLine("Text 屬性已經變成：{0}", newValue);
+    }
+
+    public void SetText()
+    {
+        publisher.Text = "Hello, event!";
+    }
+}
