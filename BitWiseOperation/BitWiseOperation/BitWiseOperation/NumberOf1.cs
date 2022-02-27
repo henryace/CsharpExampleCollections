a078c975a2adbdfad296b3743ac77983cb9cd392
henrychiang <grtert@gmail.com>
2022-02-28 00:29:57 +0800

    feature: BtiWiseOperation

---
+++ BitWiseOperation/BitWiseOperation/BitWiseOperation/NumberOf1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitWiseOperation
+{
+    public class Operation
+    {
+        public static int NumberOf1(int n)
+        {
+            var count = 0;
+            int flag = 1;
+
+            while (flag > 0)
+            {
+                if((n & flag) > 0)
+                    count++;
+
+                flag = flag << 1;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ BitWiseOperation/BitWiseOperation/BitWiseOperation/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BitWiseOperation;
+
+Console.WriteLine("Hello, World!");
+
+var result = Operation.NumberOf1(10);
+Console.WriteLine(result);
