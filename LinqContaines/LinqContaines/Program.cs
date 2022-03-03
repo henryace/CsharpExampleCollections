cb4655a4ab8b1ab99bcf970f72924eadf5b031cf
henrychiang <henrychiang@boyu66.net>
2022-03-03 18:36:54 +0800

    feature: linq container
    Signed-off-by: henrychiang <henrychiang@boyu66.net>

---
+++ LinqContaines/LinqContaines/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+var list = new List<string>() { "ASUS", "Acer", "BenQ", "Toshiba", "Dell" };
+var temp = list.Select(x => x.ToString()).Contains("Acer");
+var temp2 = list.Select(x => x.ToString()).Contains("Dell");
+var temp3 = temp && temp2;
+
+Console.WriteLine(temp3);
