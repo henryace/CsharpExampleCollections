d58ee92022a0105a2be764e13e5c8f4a679772f7
henrychiang <henrychiang@boyu66.net>
2022-03-03 18:34:08 +0800

    feature: yield practice
    Signed-off-by: henrychiang <henrychiang@boyu66.net>

---
+++ GetEvenNumByYield/GetEvenNumByYield/EvenNumByYield.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetEvenNumByYield
+{
+    public class EvenNumByYield
+    {
+        public static IEnumerable<int> getEvenNumByYield(IEnumerable<int> collection)
+        {
+            foreach (var i in collection)
+            {
+                if (i % 2 == 0) yield return i;
+            }
+        }
+
+        public static IEnumerable<int> getEvenNumByLinq(IEnumerable<int> collection)
+        {
+            return collection.ToList().Where(i => i % 2 == 0);
+        }
+
+        public static IEnumerable<int> getEvenNumByTemp(IEnumerable<int> collection)
+        {
+            var temp = new List<int>();
+            foreach (var i in collection)
+            {
+                if (i % 2 == 0) temp.Add(i);
+            }
+            return temp;
+        }
+    }
+}
---
+++ GetEvenNumByYield/GetEvenNumByYield/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GetEvenNumByYield;
+using System.Diagnostics;
+
+var myList = new List<int>();
+for (var i = 0; i < 10000000; i++)
+{
+    myList.Add(i);
+}
+
+var timeList = new List<long>();
+for (var i = 0; i < 10; i++)
+{
+    var sum = 0;
+    var sw = new Stopwatch();
+    sw.Start();
+    //var list = EvenNumByYield.getEvenNumByYield(myList);
+    var list = EvenNumByYield.getEvenNumByLinq(myList);
+    //var list = EvenNumByYield.getEvenNumByTemp(myList);
+    foreach (var l in list)
+    {
+        sum += l;
+    }
+
+    // Reuse collection
+    foreach (var l in list)
+    {
+        sum -= l;
+    }
+    sw.Stop();
+    timeList.Add(sw.ElapsedMilliseconds);
+    Console.WriteLine("Exe time: {0} ms", sw.ElapsedMilliseconds);
+}
+Console.WriteLine("Average: {0} ms", (double)timeList.Sum() / 10);
+Console.WriteLine("Memory Usage: {0}", Process.GetCurrentProcess().PrivateMemorySize64);
+
+//getEvenNumByYield
+//Average: 340.2 ms
+//Memory Usage: 157204480
+
+// getEvenNumByLinq
+// Average: 168.6 ms
+// Memory Usage: 136978432
+
+// getEvenNumByTemp
+// Average: 189.9 ms
+// Memory Usage: 187572224
+
+// Time getEvenNumByLinq < getEvenNumByTemp < getEvenNumByYield
+// Space getEvenNumByLinq < getEvenNumByTemp < getEvenNumByYield
